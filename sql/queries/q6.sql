043fd8f5026c5368bc6d551068e08b79ee5928c1
Rubayi17753 <fuchsrubayicoding@protonmail.com>
2025-11-22 10:59:33 +0700

    Fix q6

--- sql/queries/q6.sql
+++ sql/queries/q6.sql
@@ -3,7 +3,7 @@
 WITH rafsi_defs_renamed AS (
 	SELECT 
 		rafsi,
-		gismu AS cmavo,
+		gismu AS gismu_or_cmavo,
 		meaning
 FROM 
 	rafsi_defs
@@ -11,12 +11,12 @@ FROM
 
 SELECT 
 	rafsi_defs_renamed.rafsi, 
-	rafsi_defs_renamed.cmavo,
+	rafsi_defs_renamed.gismu_or_cmavo,
 	cmavo_defs.class AS bai,
 	rafsi_defs_renamed.meaning 
 FROM 
 	rafsi_defs_renamed
-		JOIN cmavo_defs ON rafsi_defs_renamed.cmavo = cmavo_defs.cmavo
-WHERE LENGTH(gismu) < 5
+		JOIN cmavo_defs ON rafsi_defs_renamed.gismu_or_cmavo = cmavo_defs.cmavo
+WHERE LENGTH(gismu_or_cmavo) < 5
 ORDER BY bai ASC, rafsi_defs_renamed.meaning ASC
 ;
