f1b71e0151637f94e2e646fc11d8b8b61749ae61
Rubayi17753 <fuchsrubayicoding@protonmail.com>
2025-11-23 08:27:48 +0700

    Fix q5b and noralujv (wrong header assigned)

--- sql/queries/q5b.sql
+++ sql/queries/q5b.sql
@@ -7,7 +7,7 @@ SELECT
     rafsi_defs.meaning,
     lujvo_component_meanings.component_meaning AS component_meaning,
     lujvo_component_meanings.assigned_meaning AS meaning_lujv1999,
-    noralujv.meaning AS meaning_noralujv
+    noralujv.meaning AS meaning_noralujv,
     CASE
         WHEN lujvo_sequence = 0 THEN 'ini'
         WHEN lujvo_sequence = lujvo_len - 1 THEN 'fin'
@@ -15,6 +15,7 @@ SELECT
     END AS position,
     lujvo_sequence,
     lujvo_len
+
 FROM lujvo_1999_concordance 
     JOIN rafsi_defs
         ON lujvo_1999_concordance.lujvo_for_split = rafsi_defs.rafsi
