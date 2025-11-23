03af36fe8b199b9915d8ec615a0874eeebde5306
Rubayi17753 <fuchsrubayicoding@protonmail.com>
2025-11-23 08:33:25 +0700

    Replace JOIN in q5b with LEFT JOIN

--- sql/queries/q5b.sql
+++ sql/queries/q5b.sql
@@ -7,7 +7,7 @@ SELECT
     rafsi_defs.meaning,
     lujvo_component_meanings.component_meaning AS component_meaning,
     lujvo_component_meanings.assigned_meaning AS meaning_lujv1999,
-    noralujv.meaning AS meaning_noralujv,
+    -- noralujv.meaning AS meaning_noralujv,
     CASE
         WHEN lujvo_sequence = 0 THEN 'ini'
         WHEN lujvo_sequence = lujvo_len - 1 THEN 'fin'
@@ -17,12 +17,15 @@ SELECT
     lujvo_len
 
 FROM lujvo_1999_concordance 
-    JOIN rafsi_defs
+    LEFT JOIN rafsi_defs
         ON lujvo_1999_concordance.lujvo_for_split = rafsi_defs.rafsi
-    JOIN lujvo_component_meanings
+    LEFT JOIN lujvo_component_meanings
         ON lujvo_1999_concordance.lujvo = lujvo_component_meanings.lujvo
-    JOIN noralujv
+    /*
+    LEFT JOIN noralujv
         ON lujvo_1999_concordance.lujvo = noralujv.lujvo
+    */
+
     /*
     LEFT JOIN lujvo_freqs_1999
         ON lujvo_1999_concordance.lujvo = lujvo_freqs_1999.
