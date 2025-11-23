fa7693b4bbf450c5b9707d7111932e8f850ce548
Rubayi17753 <fuchsrubayicoding@protonmail.com>
2025-11-23 11:04:38 +0700

    Fix component meaning generation query

--- sql/views/create_views4.sql
+++ sql/views/create_views4.sql
@@ -14,16 +14,17 @@ CREATE VIEW IF NOT EXISTS lujvo_component_meanings AS
         ),
 
         meanings AS (
-        SELECT 
-            rafsi AS form, meaning FROM rafsi_defs
-            UNION SELECT gismu AS form, meaning FROM rafsi_defs     
-            UNION SELECT substring(gismu, 1, LENGTH(gismu)) AS form, meaning FROM rafsi_defs     
+            SELECT rafsi AS form, meaning FROM rafsi_defs 
+            UNION
+            SELECT gismu, meaning FROM gismu_defs 
+            UNION   
+            SELECT substring(gismu, 1, LENGTH(gismu)), meaning FROM gismu_defs     
         )
 
     SELECT
         lujvo,
         lujvo_parsed,
-        GROUP_CONCAT(meanings.meaning,'-') AS component_meaning,
+        GROUP_CONCAT(meanings.meaning, '-') AS component_meaning,
         lujvo_freqs_1999.canon_meaning AS assigned_meaning
     FROM lujvo_1999_concordance_distinct 
         LEFT JOIN meanings
