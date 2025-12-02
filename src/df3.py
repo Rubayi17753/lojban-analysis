1772d82fea94768c6b6475a195817520685164eb
Rubayi17753 <fuchsrubayicoding@protonmail.com>
2025-12-02 16:14:40 +0700

    Place the main filter in df3 in its own function for easier handling

--- src/df3.py
+++ src/df3.py
@@ -3,26 +3,56 @@ import pandas as pd
 import src.inserts as inserts
 from src.df1_q1 import main as dfq1
 from src.df1_q2 import main as dfq2
+from src.tools.class_table import Table
+
+def filters():
+
+    mask = (
+        (df['coef1'] > 2)   # Coefficient here: q2's df['cmavo_rafsi_freq'] / df['max_form_freq'] * 100
+        & (~df['rafsi_pos'].str.contains('_'))
+    )
+    return mask
 
 def filter_and_aggregate(df):
 
-    mask = (
-        df['coef1'] > 2   # Coefficient here: q2's df['form_freq'] / df['max_form_freq'] * 100
-        & df['rafsi_pos'].str.contains('_')
-    )
+    mask = filters()
 
     df = df.assign(
-                include=df['cmavo_rafsi'].where(mask, ' '),
-                exclude=df['cmavo_rafsi'].where(~mask, ' '),
+                form=df['cmavo_rafsi'].where(mask, ' '),
+                form_x=df['cmavo_rafsi'].where(~mask, ' '),
+                rafsi_pos=df['rafsi_pos'].where(mask, ' '),
+                form_shape=df['form_shape'].where(mask, ' '),
             ).groupby('gismu', as_index=False).agg(
-                forms_included=('include', ' '.join),
-                forms_excluded=('exclude', ' '.join),
+                form=('form', ' '.join),
+                form_x=('form_x', ' '.join),
+                rafsi_pos=('rafsi_pos', ' '.join),
+                form_shape=('form_shape', ' '.join),
     )
+
+    df['rafsi_pos'] = df['rafsi_pos'].str.strip()
+    df['form_shape'] = df['form_shape'].str.strip()
+
+    return df
+
+def produce_pivot(df):
+
+    df2 = Table('defs_rafsi', keep_default_na=False).dff
+
+    # Check if df.gismu/cmavo_rafsi is present in df2.gismu/rafsi. If not, designate as CAA2
+    df2_index = df2.set_index(['gismu', 'rafsi']).index
+    mask = df.set_index(['gismu', 'cmavo_rafsi']).index.isin(df2_index)
+    mask2 = df['form_shape'].isin(['CACC', 'CCAC', 'CACCA', 'CCACA'])
+    df.loc[~mask & ~mask2, 'form_shape'] = 'CAA2'
+
+    print(df[df.duplicated(subset=['gismu', 'form_shape'], keep=False)][['gismu', 'form_shape', 'cmavo_rafsi']])
+    exit()
+
+    df = df.pivot(index='gismu', columns='form_shape', values='cmavo_rafsi')
 
     return df
 
 def determine_pos_tendency(df):
-    # Determine rafsi positioning tendency.
+    # Determine rafsi positioning tendency
     # Requires dfq1() --> coef2
 
     # Sanitisation
@@ -56,13 +86,16 @@ def main():
     # Aggregation
     df = filter_and_aggregate(df)
 
-    # Post-aggregation
+    # Merge dfq1 and pivot(dfq2)
     df = df.merge(dfq1(),
+            on='gismu', how='left').merge(produce_pivot(dfq2()),
             on='gismu', how='left')
 
     df = determine_pos_tendency(df)
     df = df.sort_values(by=['gismu_shape', 'as_rafsi'], ascending=[True, False])
-    df = df[['gismu', 'pos_tendency', 'forms_included', 'forms_excluded']]
+    '''df = df[['gismu', 'gismu_shape', 
+            'pos_tendency', 
+            'form', 'form_x', 'rafsi_pos', 'form_shape']]'''
     df = inserts.insert_meanings(df)
     df.to_csv('results/df3.csv', sep=',', index=False)
     return df
--- tests/test1.py
+++ tests/test1.py
@@ -3,4 +3,4 @@ from src.substring_positions import substring_positions
 def main():    
     print(substring_positions('berda', 'bre', out='string'))
     print(substring_positions('berde', 'bre', out='string'))
-    print(substring_positions('cusku', 'bre', out='string'))
+    print(substring_positions('cusku', 'cusku', out='string'))
